c52aace5adebe793396b9ee8e53c0ebb1ff88376
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2014-08-19 15:16:43 +0200

    Initial commit, grunt-init gruntplugin

---
+++ tasks/batch.js
@@ -0,0 +1,30 @@
+/*
+ * grunt-batch
+ * https://github.com/alessandro-pezzato/grunt-batch
+ *
+ * Copyright (c) 2014 Alessandro Pezzato
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var exec = require('child_process').exec;
+var async = require('async');
+
+module.exports = function (grunt) {
+  grunt.registerMultiTask('batch', 'Easy way to run a shell command for each file.', function () {
+    var options = this.options({});
+    var series = [];
+    this.files.forEach(function (f) {
+      var cmd = options.cmd(f);
+      series.push(function (callback) {
+        exec(cmd, function () {
+          grunt.log.ok(f.src, ' => ', f.dest);
+          callback();
+        });
+      });
+    });
+    var done = this.async();
+    async.series(series, done);
+  });
+};
